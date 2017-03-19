8a2fea0620efd38b7da43af46bf8b71fa54dffc3
rahulrsingh09 <rahulrsingh09@gmail.com>
2017-03-19 11:54:24 +0530

    Creating Test for Pipe Components and Updated to Angular 4 Rc-5

--- src/app/pipes/pipes.component.spec.ts
+++ src/app/pipes/pipes.component.spec.ts
@@ -1,9 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PipesComponent } from './pipes.component';
+import {FormsModule} from "@angular/forms";
+
+
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'square'})
+class MockPipe implements PipeTransform {
+  transform(value: number): number {
+    //Do stuff here, if you want
+    return value;
+  }
+}
+
 
 describe('PipesComponent', () => {
   let component: PipesComponent;
   let fixture: ComponentFixture<PipesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule ],
+      declarations: [ PipesComponent,MockPipe ], // declare the test component
+    })
+      .compileComponents().then(()=>TestBed.createComponent(PipesComponent));  // compile template and css
+  }));
+
+  fixture = TestBed.createComponent(PipesComponent);
+  component = fixture.componentInstance;
+
+
+  it('should have a defined component', () => {
+    expect(component).toBeDefined();
+  });
 
 });
