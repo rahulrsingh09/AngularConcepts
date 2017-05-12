e0020943971d4ba52cff14e42761207914227bbd
rahulrsingh09 <rahulrsingh09@gmail.com>
2017-05-12 14:54:40 +0530

    Angular Animations and Ngrx Store update Work Related Info in Additional

--- src/app/ngrx/ngrx.component.ts
+++ src/app/ngrx/ngrx.component.ts
@@ -8,7 +8,7 @@ import {
   state,
   style,
   animate,
-  transition
+  transition, group
 } from '@angular/animations';
 
 interface AppState {
@@ -33,25 +33,51 @@ interface AppState {
       transition('inactive => active', animate('100ms ease-in')),
       transition('active => inactive', animate('100ms ease-in')),
     ]),
-    trigger('flyInOut', [
+    //another type of styling
+    /*trigger('flyInOut', [
       state('in', style({transform: 'translateX(0)'})),
-      transition('void => *', [
+      transition(':enter', [
         style({transform: 'translateX(-100%)'}),
-        animate('300ms ease-in')
-      ]),
+          animate('300ms ease-in')
+        ]),
       transition('* => void', [
         animate(300, style({transform: 'translateX(100%)'}))
+      ])
+    ])*/trigger('flyInOut', [
+      state('in', style({width: 120, transform: 'translateX(0)', opacity: 1})),
+      transition('void => *', [
+        style({width: 10, transform: 'translateX(50px)', opacity: 0}),
+        group([
+          animate('0.3s 0.1s ease', style({
+            transform: 'translateX(0)',
+            width: 1140
+          })),
+          animate('0.3s ease', style({
+            opacity: 1
+          }))
+        ])
+      ]),
+      transition('* => void', [
+        group([
+          animate('0.3s ease', style({
+            transform: 'translateX(50px)',
+            width: 10
+          })),
+          animate('0.3s 0.2s ease', style({
+            opacity: 0
+          }))
+        ])
       ])
     ])
 
 
   ]
 })
-export class NgrxComponent implements OnInit,OnChanges {
+export class NgrxComponent implements OnInit, OnChanges {
 
   counter: Observable<number>;
-  item : Observable<string[]>;
-  state:string = 'inactive';
+  item: Observable<string[]>;
+  state: string = 'inactive';
 
 
   constructor(private store: Store<AppState>){
@@ -84,7 +110,11 @@ export class NgrxComponent implements OnInit,OnChanges {
   }
 
   ngOnChanges(){
-    console.log("Item value"+ this.item);
+    console.log("Item value" + this.item);
+  }
+
+  animationStarted() {
+    console.log('Started Animation');
   }
 
 }
