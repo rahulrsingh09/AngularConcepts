bce440cd742dd1ffdc8dfbe4682f496f6d859874
rahulrsingh09 <rahulrsingh09@gmail.com>
2017-05-14 16:19:25 +0530

    Custom Validator in model

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit, HostListener} from '@angular/core';
+import {Component, OnInit, HostListener, ViewChild, AfterViewChecked} from '@angular/core';
 
 import {User} from "../shared/user.interface";
 import {Theme} from "../shared/theme.interface";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-template-driven',
@@ -9,11 +10,63 @@ import {Theme} from "../shared/theme.interface";
   styleUrls: ['./template-driven.component.css'],
   host: {'(click)': 'onClick()'}
 })
-export class TemplateDrivenComponent implements OnInit {
+export class TemplateDrivenComponent implements OnInit,AfterViewChecked {
+
+  // all field related validation moved to the backend model instead of the template
+  f: NgForm;
+  @ViewChild('f') currentForm: NgForm;
+
+  ngAfterViewChecked(){
+      this.formChanged();
+  }
+
+
+  formChanged() {
+    if (this.currentForm === this.f) { return; }
+    this.f = this.currentForm;
+    if (this.f) {
+      this.f.valueChanges
+        .subscribe(data => this.onValueChanged(data));
+    }
+  }
+
+
+  onValueChanged(data?: any) {
+    if (!this.f) { return; }
+    const form = this.f.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = {
+    'name': '',
+    'power': ''
+  };
+
+  validationMessages = {
+    'name': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be at least 4 characters long.',
+      'nameRahul': 'Name Must be Rahul'
+    }
+  };
+
 
   @HostListener('click') onClick(){
     console.log("User Click using Host Listner");
   }
+
 
   public user:User;
 
