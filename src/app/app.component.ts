e28d4f2063b2d6a915c70f6e1fc61ec3fedc2aab
rahulrsingh09 <rahulrsingh09@gmail.com>
2017-07-26 12:01:38 +0530

    Upcomming changes V3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,8 +36,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit() {
     this.subscription = this.service.spinner$
-      .subscribe(item => this.showProgress = item);
-    console.log(this.showProgress);
+      .subscribe(item => setTimeout(() => this.showProgress = item, 0)); // work around for ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked
     this.service.incrementPageCount().then(data => this.service.changeCount(data));
 
   }
