14a7a8eba3993c489b0a0d3c83ace2bfeecd4129
Rahul Singh <rahulrsingh09@gmail.com>
2017-08-14 16:12:38 +0530

    Arrange

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { ValidateOnBlurDirective } from './reactive/validateOnBlur.directive';
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
@@ -6,16 +5,31 @@ import {HttpModule} from "@angular/http";
 import {RouterModule} from "@angular/router";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {HashLocationStrategy, LocationStrategy, PathLocationStrategy} from "@angular/common";
+import {StoreModule} from "@ngrx/store";
+import {ShareButtonsModule} from "ngx-sharebuttons";
+import {MomentModule} from "angular2-moment";
+import {MaterialModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ChartModule} from "angular2-highcharts";
+import {LocalStorageModule} from "angular-2-local-storage";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {AngularFireModule} from "angularfire2";
+import {NgxPaginationModule} from "ngx-pagination";
+import {HighchartsStatic} from "angular2-highcharts/dist/HighchartsService";
+import {ModalModule} from "angular2-modal";
+import {BootstrapModalModule} from "angular2-modal/plugins/bootstrap";
+import {AngularFireAuth} from "angularfire2/auth/auth";
+import {AngularFireDatabaseModule} from "angularfire2/database/database.module";
+
+
 import {AppComponent, DialogResultExampleDialog} from "./app.component";
 import {AngularService} from "./shared/angular.service";
 import {NgrxComponent} from "./ngrx/ngrx.component";
-import {StoreModule} from "@ngrx/store";
 import {routing} from "./app.routing";
 import {TemplateDrivenComponent} from "./template-driven/template-driven.component";
 import {ReactiveComponent} from "./reactive/reactive.component";
 import {Angular4Component} from "./angular4/angular4.component";
 import {Tabs} from "./childrenEg/tabs";
-import {Tab} from "./childrenEg/tab";
 import {ViewChildContentEgComponent} from "./childrenEg/view-child-content-eg.component";
 import {ParentHostComponent} from "./parent-host/parent-host.component";
 import {ChildComponent} from "./parent-host/child.component";
@@ -31,15 +45,8 @@ import {DropdownDirective} from "./shared/dropdown.directive";
 import {ViewencapsulationComponent} from "./viewencapsulation/viewencapsulation.component";
 import {ParentchildComponent} from "./parentchild/parentchild.component";
 import {ChildparentComponent} from "./parentchild/childparent.component";
-import {MomentModule} from "angular2-moment";
-import {MaterialModule} from "@angular/material";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AngularcliComponent} from "./angularcli/angularcli.component";
 import {BasicComponent} from "./basic/basic.component";
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {LocalStorageModule} from "angular-2-local-storage";
-import {ChartModule} from "angular2-highcharts";
-import {HighchartsStatic} from "angular2-highcharts/dist/HighchartsService";
 import {ChartsComponent} from "./charts/charts.component";
 import {NameValidatorDirective} from "./template-driven/NameValidator";
 import {AsyncAgeValidator} from "./template-driven/AsyncAgeValidator";
@@ -49,24 +56,19 @@ import {HeroJobAdComponent} from "./dynamic-component/hero-job-ad.component";
 import {HeroProfileComponent} from "./dynamic-component/hero-profile-ad.component";
 import {AdBannerComponent} from "./dynamic-component/ad-banner.component";
 import {NotesComponent} from "./notes/notes.component";
-import {AngularFireModule} from "angularfire2";
-import {AngularFireDatabaseModule} from "angularfire2/database/database.module";
-import {AngularFireAuth} from "angularfire2/auth/auth";
 import {CommentComponent} from "./comment/comment.component";
 import {FireAuthResolve} from "./comment/fireauth.resolve";
 import {TestService} from "./shared/test.service";
 import {TestComponent} from "./test/test.component";
 import {RelativeTimeFilterPipe} from "./comment/realtiveTime.filter.pipe";
-import {ModalModule} from "angular2-modal";
-import {BootstrapModalModule} from "angular2-modal/plugins/bootstrap";
-import {NgxPaginationModule} from "ngx-pagination";
 import {StringFilterPipe} from "./comment/string.filter.pipe";
 import {firebaseConfig} from './shared/firebase.config';
 import {firebaseConfigDev} from './shared/firebase.config';
-import { FirebaseComponent } from './firebase/firebase.component';
-import {ShareButtonsModule} from "ngx-sharebuttons";
 import { MyworkComponent } from './mywork/mywork.component';
+import { FirebaseComponent } from './firebase/firebase.component';
 import * as fromRoot from "./ngrx/state-management/ngrx-reducer";
+import { ValidateOnBlurDirective } from './reactive/validateOnBlur.directive';
+import {Tab} from "./childrenEg/tab";
 
 
 export function highchartsFactory() {
@@ -140,7 +142,7 @@ export function highchartsFactory() {
     MaterialModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    AngularFireModule.initializeApp(firebaseConfigDev),
+    AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
     LocalStorageModule.withConfig({
       prefix: 'my-app',
