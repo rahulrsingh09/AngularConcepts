e0b6ddb7d96cbc63ed1a819f5cfb55cdf735c52b
Rahul Singh <rahulrsingh09@gmail.com>
2017-08-24 00:51:13 +0530

    Services Component

--- src/app/services/fake.service.ts
+++ src/app/services/fake.service.ts
@@ -1,8 +1,12 @@
 import { Headers } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {HttpParams} from "@angular/common/http";
 
 const headers = new HttpHeaders().set("content-Type", "application/json");
+
+const params = new HttpParams()
+    .set('page', '2');
 
 interface User {
     id: number;
@@ -17,11 +21,26 @@ export class FakeService{
     constructor(private httpClient : HttpClient){}
 
     get(){
-        return this.httpClient.get<User[]>('https://reqres.in/api/users?page=2').map(value => value['data']);
+        return this.httpClient.get<User[]>('https://reqres.in/api/users', {params}).map(value => value['data']);
     }
 
     post(body:Object){
        // headers.append('authentication', `${student.token}`);    if any auth token
         return this.httpClient.post<any[]>('https://reqres.in/api/users',body,{headers});
     }
+
+    put(body: Object){
+        return this.httpClient.post<Object>('https://reqres.in/api/users/2',body,{headers});
+    }
+    
+    patch(body: Object){
+        return this.httpClient.patch<Object>('https://reqres.in/api/users/2',body,{headers});
+    }
+
+    delete(body: Object){
+        return this.httpClient.delete('https://reqres.in/api/users/2',{headers});
+    }
+
+    
+
 }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -11,17 +11,26 @@ import { User } from './../shared/user.interface';
 export class ServicesComponent implements OnInit {
 
   users:User[];
-  body = {
+  post = {
       "name": "morpheus",
       "job": "leader"
     };
+  put = {
+    "name": "morpheus",
+    "job": "zion resident"
+  };
+  patch = {
+    "name": "rahul"
+  };
   postData:Object;
 
   constructor(private fake : FakeService) { }
 
   ngOnInit() {
-    this.fake.get().subscribe(data => this.users = data);
-    this.fake.post(this.body).subscribe( data => this.postData = data);
+    this.fake.get().subscribe(data =>console.log(data));
+    this.fake.post(this.post).subscribe( data => this.postData = data);
+    this.fake.put(this.put).subscribe( data => console.log(data));
+    this.fake.patch(this.patch).subscribe( data => console.log(data));
   }
 
 
