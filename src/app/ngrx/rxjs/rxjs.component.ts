4eb7e5301670229545eb1fe8eacced4f633befb8
Rahul Singh <rahulrsingh09@gmail.com>
2017-09-02 03:35:28 +0530

    Small bug

--- src/app/ngrx/rxjs/rxjs.component.ts
+++ src/app/ngrx/rxjs/rxjs.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  back(){
+    //console.log("called");
+    this.router.navigate(['ngrx']);
   }
 
 }
