4905cf5a8fbd90cf4912e5497f29d0d4dddf670e
Rahul Singh <rahulrsingh09@gmail.com>
2017-09-30 00:52:59 +0530

    Lazy Loading Angular

--- src/app/ngrx/rxjs/rxjs.component.ts
+++ src/app/ngrx/rxjs/rxjs.component.ts
@@ -1,21 +1,17 @@
 
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { MdDialog, MdDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html'
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit{
 
   constructor(private router : Router, public dialog: MdDialog) { }
 
-  ngOnInit() {
-    let dialogRef = this.dialog.open(LazyDailog);
-    dialogRef.afterClosed().subscribe(result => {
-    });
-  }
+  ngOnInit(){setTimeout(() => this.dialog.open(LazyDailog),0);}
 
   back(){
     
