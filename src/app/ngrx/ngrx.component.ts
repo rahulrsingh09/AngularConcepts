015b5844a02683ea310a771487cfe1e1a061ed80
Rahul Singh <rahulrsingh09@gmail.com>
2017-09-30 13:12:59 +0530

    Info

--- src/app/ngrx/ngrx.component.ts
+++ src/app/ngrx/ngrx.component.ts
@@ -20,11 +20,9 @@ import {INCREMENT, DECREMENT, RESET, ADD, DEL} from './state-management/ngrx-act
   animations: [
     trigger('ngrxStore',[
       state('inactive', style({
-        backgroundColor: '#eee',
         transform: 'scale(1)'
       })),
       state('active', style({
-        backgroundColor: '#cfd8dc',
         transform: 'scale(1.1)'
       })),
       transition('inactive => active', animate('100ms ease-in')),
