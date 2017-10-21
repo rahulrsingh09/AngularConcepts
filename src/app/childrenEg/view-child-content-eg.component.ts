3c4c7649e36184fbadc19bec2d47c221b08ec896
Rahul Singh <rahulrsingh09@gmail.com>
2017-10-21 12:39:53 +0530

    View Child updated

--- src/app/childrenEg/view-child-content-eg.component.ts
+++ src/app/childrenEg/view-child-content-eg.component.ts
@@ -9,29 +9,7 @@ import {Tab} from './tab';
 @Component({
 
   selector: 'view-child-content',
-  template: `
-      
-    <div class="row">
-      <div class="col-md-12">
-        <p class="myHeader">
-          View Child & Children + Some Notes on Angular Common Questions :D<span mdTooltip="View Source">
-        <a href="https://github.com/rahulrsingh09/AngularConcepts/tree/master/src/app/childrenEg">
-          <img src="code.png" alt="Image">
-        </a>
-      </span>
-        </p>
-      </div>
-    </div>
-    <hr>
-    <tabs>
-       <tab [tabTitle]="'View Child Tab'" (click) = "update()"><p>Click on me & see Console Log Press F12</p></tab>
-       <tab [tabTitle]="'Test View Children Tab'"><p>Data inside Tab can add component Also</p></tab>
-    </tabs>
-    <div class="small">
-    <div #child>Check Console for View Child Example
-    <div>Check Console for View Children Example</div></div>
-    </div>
-  `,
+  templateUrl: './view-child.html',
   styles : ['.small { font-size: 10px; }']
 })
 
